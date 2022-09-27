c7059f5ba304c52c4dd01b5f60c751f73ba45b0f
Apoorv-sr <109285079+Apoorv-sr@users.noreply.github.com>
2022-09-27 14:56:54 +0530

    Update pass-props-to-a-stateless-functional-component.jsx

--- pass-props-to-a-stateless-functional-component.jsx
+++ pass-props-to-a-stateless-functional-component.jsx
@@ -30,3 +30,4 @@ class Calendar extends React.Component {
 
 //Note that we can create react components using normal javascript function without any class or by writing a class and the class name becomes the react component name and whatever we return from the class or the function is the JSX element,in the above example we can see that CurrentDate and Calendar both are react components but Calendar is declared using class and CurrentDate as a normal javascript function.
 //Note that the CurretDate is called "stateless functional Component" and Calendar is called an "ES6 class component" but both of them are methods to create react components,the class components are declared so that some special fetures can be utilised.
+//Note that in stateless component we access a props as props.name but in class components we access the props as this.props.name i.e. we have to use this keyword in class components while accessing props attrtibutes.
